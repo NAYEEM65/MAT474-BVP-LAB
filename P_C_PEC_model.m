218c8bc3daa71a59f85fc90b8e080205452b65cd
NAYEEM65 <mathnayeem054@gmail.com>
2023-03-13 09:40:57 +0600

    P_C_PEC_model modified

--- P_C_PEC_model.m
+++ P_C_PEC_model.m
@@ -1,7 +1,8 @@
-% Solve the IVP Dy=y-x^2 y(0)=1 by using 4-step adam bashforth method
+% Solve the IVP Dy=y-x^2 y(0)=1 by using P_C method in p(EC)^m
+%mode
 % with h=0.1, where D is the differential operator with respect to x
 %
-function y_c=P_C_PEC_model(a,h,n,y0)
+function y_c=P_C_PEC_model(a,h,n,y0,M)
 x=a:h:n*h;
 f=inline('y-x^2');
 for i=1:1:3
@@ -11,9 +12,13 @@ end
 y_c=y_p;
 for i=3:1:n
     y_p(i+1)=y_p(i)+(h/12)*(23*f_eva(i)-16*f_eva(i-1)+5*f_eva(i-2));
-    f_eva(i+1)=f(x(i+1),y_p(i+1));
+    for m=1:1:M
+        f_eva(i+1)=f(x(i+1),y_p(i+1));
    y_p(i+1)=y_p(i)+(h/12)*(5*f_eva(i+1)+8*f_eva(i)-f_eva(i-1));
-   y_p=y_c;
+   y_p=y_c; 
+    end
+     fprintf('\t\ny(%0.2f)=%0.5f', x(i+1), y_c(i+1));
+   
 end
 y_exact = exp(x)+x.^2+2*x+2;
 plot(x,y_c,'r-*',x,y_exact,'g');
